29e09cc36223a7c8e63033f2bf6cd77dc9d04ce6
boomboxik <9067506@gmail.com>
2023-11-03 20:09:26 +0300

    added socket-io-adapter updated main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,19 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SocketIOAdapter } from './socket-io-adapter';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
+    
 
   const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT'));
   const clientPort = parseInt(configService.get('CLIENT_PORT'));
   
   app.enableCors({
@@ -23,7 +19,8 @@ async function bootstrap() {
       new RegExp('/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/'),
     ],
   });
-  const port = parseInt(configService.get('PORT'));
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
+  
   await app.listen(port);
 
   logger.log(`Server running on port ${port}`);
