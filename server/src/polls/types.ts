6c5d5d0432482b58189b3b2dfc75f1aa5e4da5d0
boomboxik <9067506@gmail.com>
2023-11-06 19:21:04 +0300

    updated polls.service.ts, types.ts, polls.repository

--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -18,6 +18,17 @@ export type CreatePollFields = {
     userID: string;
     name: string;
   };
+  
+  export interface AddParticipantFields {
+    pollID: string;
+    userID: string;
+    name: string;
+  }
+
+  export interface RemoveParticipantData {
+    pollID: string;
+    userID: string;
+  }
 
   // repository types
   export type CreatePollData = {
