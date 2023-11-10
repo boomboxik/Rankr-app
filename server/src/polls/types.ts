76789737d794fd16b9f0b32bab86e882a7f5ef0c
boomboxik <9067506@gmail.com>
2023-11-10 20:02:00 +0300

    updated polls.repository and poll-types.ts

--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -4,55 +4,67 @@ import { Socket } from 'socket.io';
 
 // service types
 export type CreatePollFields = {
-    topic: string;
-    votesPerVoter: number;
-    name: string;
-  };
-  
-  export type JoinPollFields = {
-    pollID: string;
-    name: string;
-  };
-  
-  export type RejoinPollFields = {
-    pollID: string;
-    userID: string;
-    name: string;
-  };
-  
-  export type AddParticipantFields = {
-    pollID: string;
-    userID: string;
-    name: string;
-  }
+  topic: string;
+  votesPerVoter: number;
+  name: string;
+};
 
-  export type AddNominationFields = {
-    pollID: string;
-    userID: string;
-    text: string;
-  };
+export type JoinPollFields = {
+  pollID: string;
+  name: string;
+};
 
-  // repository types
-  export type CreatePollData = {
-    pollID: string;
-    topic: string;
-    votesPerVoter: number;
-    userID: string;
-  };
-  
-  export type AddParticipantData = {
-    pollID: string;
-    userID: string;
-    name: string;
-  };
+export type RejoinPollFields = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
 
-  export type AddNominationData = {
-    pollID: string;
-    nominationID: string;
-    nomination: Nomination;
-  };
+export type AddParticipantFields = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
 
-  // guard types
+export type AddNominationFields = {
+  pollID: string;
+  userID: string;
+  text: string;
+};
+
+export type SubmitRankingsFields = {
+  pollID: string;
+  userID: string;
+  rankings: string[];
+};
+
+// repository types
+export type CreatePollData = {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+};
+
+export type AddParticipantData = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
+
+export type AddNominationData = {
+  pollID: string;
+  nominationID: string;
+  nomination: Nomination;
+};
+
+export type AddParticipantRankingsData = {
+  pollID: string;
+  userID: string;
+  rankings: string[];
+};
+
+// guard types
 export type AuthPayload = {
   userID: string;
   pollID: string;
